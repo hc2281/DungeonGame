eea626733d8242d6f3d44bb4cbf30d3efce5aa08
zll19 <zll19@ic.ac.uk>
2022-11-22 20:38:21 +0000

    nimated cauldron

--- Assets/Scripts/CraftingManager.cs
+++ Assets/Scripts/CraftingManager.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CraftingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Stores item that's being dropped
+    private Item currentItem;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    public void OnMouseDownItem(Item item){
+        if(currentItem == null){
+            currentItem = item;
+        }
+    } 
 }
