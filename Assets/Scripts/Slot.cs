18eb72ed135e1aef8e66a9f4e331b8ef9de75377
zll19 <zll19@ic.ac.uk>
2022-11-22 13:13:08 +0000

    background scene

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    //stores what item is inside
+    public Item item;
+    //
+    public int index;
+}
