595e38d1af0fd3497c01da72620ce1132f2575d9
zll19 <zll19@ic.ac.uk>
2022-11-29 15:59:17 +0000

    Rudimentary potion completion

--- Assets/Scripts/CraftingManager.cs
+++ Assets/Scripts/CraftingManager.cs
@@ -11,6 +11,11 @@ public class CraftingManager : MonoBehaviour
 
     public Newslot[] craftingSlots;
 
+    public List<Item> itemList;
+    public string[] recipes;
+    public Item[] recipeResults;
+    public Newslot resultSlot;
+
     private void Update()
     {
         if (Input.GetMouseButtonUp(0)){
@@ -30,9 +35,41 @@ public class CraftingManager : MonoBehaviour
                 nearestSlot.gameObject.SetActive(true);
                 nearestSlot.GetComponent<Image>().sprite = currentItem.GetComponent<Image>().sprite;
                 nearestSlot.item = currentItem;
+                itemList[nearestSlot.index] = currentItem;
                 currentItem = null;
+                CheckForCreatedRecipes();
             }
         }
+    }
+
+    void CheckForCreatedRecipes(){
+        resultSlot.gameObject.SetActive(false);
+        resultSlot.item = null;
+
+        string currentRecipeString = "";
+        foreach(Item item in itemList){
+            if (item != null){
+                currentRecipeString += item.itemName;
+            } 
+            else {
+                currentRecipeString += "null";
+            }
+        }
+
+        for (int i = 0; i < recipes.Length; i++){
+            if(recipes[i] == currentRecipeString){
+                resultSlot.gameObject.SetActive(true);
+                resultSlot.GetComponent<Image>().sprite = recipeResults[i].GetComponent<Image>().sprite;
+                resultSlot.item = recipeResults[i];
+            }
+        }
+    }
+
+    public void OnClickSlot(Newslot slot){
+        slot.item = null;
+        itemList[slot.index] = null;
+        slot.gameObject.SetActive(false);
+        CheckForCreatedRecipes();
     }
 
     // Takes in a parameter when item
